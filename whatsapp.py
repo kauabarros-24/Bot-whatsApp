234b4ef59d1e99eb920da7cb2ccc8bb936ea2d7e
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-17 11:59:08 -0300

    fixed: python está funcionado | fix: Instalar o selenium adequadamente

--- whatsapp.py
+++ whatsapp.py
@@ -1,11 +1,11 @@
 #Bibliotecas
 from selenium import webdriver
-import time
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 #Ir para o zap
 driver = webdriver.Chrome(ChromeDriverManager.install())
 driver.get('https://web.whatsapp.com/')
 time.sleep(30)
 #Buscar contatos
 contatos = ['Teste bot', 'Pai']
-#Enviar mensagens
+#Enviar mensagensd
